2bedec62eda9ca09255513f12fdccad2dc6dbb76
lwz <905328134@qq.com>
2026-04-19 17:12:07 +0800

    debug

--- train_llava15_lora_llava_779k_demo_10000.sh
+++ train_llava15_lora_llava_779k_demo_10000.sh
@@ -4,6 +4,7 @@ WANDB_PROJECT=mllm-lightning python src/train.py examples/train_lora/llava15_lor
   dataset_dir=data/llava_779k_demo_10000 \
   dataset=llava_779k_demo_10000_parquet \
   output_dir=saves/llava-1.5-7b/lora/llava-779k-demo-10000 \
+  run_name=llava15-lora-llava-779k-demo-10000 \
   per_device_train_batch_size=1 \
   gradient_accumulation_steps=8 \
   num_train_epochs=1.0 \
