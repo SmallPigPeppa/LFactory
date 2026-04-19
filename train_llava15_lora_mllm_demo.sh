4fbbdf4ed6a6d771fd34b6a5431057bf46ff594c
lwz <905328134@qq.com>
2026-04-19 10:15:58 +0800

    debug

--- train_llava15_lora_mllm_demo.sh
+++ train_llava15_lora_mllm_demo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python src/train.py examples/train_lora/llava15_lora_mllm_demo.yaml
+WANDB_PROJECT=mllm-lightning python src/train.py examples/train_lora/llava15_lora_mllm_demo.yaml
