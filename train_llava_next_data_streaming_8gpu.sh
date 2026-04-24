ddf05bcf2434f984a7083e96c9c6bcefb4a69e77
lwz <905328134@qq.com>
2026-04-24 16:25:18 +0800

    Minimize LLaVA-NeXT training launcher

--- train_llava_next_data_streaming_8gpu.sh
+++ train_llava_next_data_streaming_8gpu.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-cd "$(dirname "$0")"
-
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export NPROC_PER_NODE=8
 export FORCE_TORCHRUN=1
-export PYTHONPATH="$PWD/src:$PYTHONPATH"
+export PYTHONPATH=src:$PYTHONPATH
 
-python -m llamafactory.cli train examples/train_lora/llava_next_data_streaming_sft.yaml "$@"
+python -m llamafactory.cli train examples/train_lora/llava_next_data_streaming_sft.yaml
