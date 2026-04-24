e3450d6c5acfefa404f3227c224420be1002f648
lwz <905328134@qq.com>
2026-04-24 17:20:41 +0800

    debug

--- examples/train_lora/llava779k_qwen3vl2b/train.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
-
-llamafactory-cli train examples/train_lora/llava779k_qwen3vl2b/train.yaml
---
+++ train_llava779k_qwen3vl2b.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+PYTHONPATH=src python -m llamafactory.cli train examples/train_lora/llava779k_qwen3vl2b/train.yaml
