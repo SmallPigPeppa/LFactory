8fc479c9c34ab2033224c7804c96c234144f8857
lwz <905328134@qq.com>
2026-04-24 18:02:27 +0800

    debug

--- train_llava779k_qwen3vl2b.sh
+++ train_llava779k_qwen3vl2b.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONPATH=src python -m llamafactory.cli train examples/train_lora/llava779k_qwen3vl2b/train.yaml
+FORCE_TORCHRUN=1 PYTHONPATH=src python -m llamafactory.cli train examples/train_lora/llava779k_qwen3vl2b/train.yaml
